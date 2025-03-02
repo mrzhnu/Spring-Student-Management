9f3335536f3a6cdd2443176c6b49cac310503a02
mrzhnu <mrzhnu@gmail.com>
2025-03-02 23:26:36 +0500

    Updated project

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,17 +9,17 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        // Инициализация ленивого бина
+
         context.getBean(StudentReportService.class);
 
-        // Получаем основной сервис (@Primary)
+
         StudentService primaryService = context.getBean(StudentService.class);
         primaryService.addStudent(1, "Aizere");
         primaryService.addStudent(2, "Bekzhan");
         System.out.println("Student 1: " + primaryService.getStudent(1));
         System.out.println("Student 2: " + primaryService.getStudent(2));
 
-        // Получаем альтернативный сервис по имени
+        
         StudentService altService = context.getBean("studentServiceAltImpl", StudentService.class);
         altService.addStudent(3, "Aizhan");
         altService.addStudent(4, "Serzhan");
